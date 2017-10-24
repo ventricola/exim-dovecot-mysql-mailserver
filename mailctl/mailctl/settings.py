f4728448ee05f476da52f3e8b8bd85072cacf988
root <root@dec.net.ua>
2017-10-24 13:07:45 +0300

    sync confs

--- mailctl/mailctl/settings.py
+++ mailctl/mailctl/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'maildb',                      # Or path to database file if using sqlite3.
-        'USER': 'mailnull',                      # Not used with sqlite3.
+        'USER': 'mail',                      # Not used with sqlite3.
         'PASSWORD': '1234567890',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
